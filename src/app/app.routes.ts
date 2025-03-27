1e368368cd1ab4e437b07ad71df6b849986f9908
sameer <sameerkhan9991303458@gmail.com>
2025-03-27 22:14:31 +0530

    abc

---
+++ src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
+export const routes: Routes = [
+    // default page 
+    { path: '', component: DashboardComponent }
+];
---
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.scss',
+})
+export class DashboardComponent {
+
+}
